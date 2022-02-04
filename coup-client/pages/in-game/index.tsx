c7581d4950482b7bb22606a6c9e2bdb19ea8a775
liang.chen <liang.chen@thoughtworks.com>
2022-02-04 12:15:19 +0800

    [liang]next js set up

--- coup-client/pages/in-game/index.tsx
+++ coup-client/pages/in-game/index.tsx
@@ -1,12 +1,10 @@
 import { Container } from "@mui/material";
 import { Header } from "../../components/in-game/Header";
-import { UserInfos } from "../../components/in-game/UserInfos";
 
 function InGame() {
     return (
         <Container maxWidth="xs">
             <Header />
-            <UserInfos />
         </Container>
     );
 }
